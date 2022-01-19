533bfe106b67f0dfbd40f5c41e9dcbeeeadd3b8d
ShrutiBelsare27 <shrutibelsare27@gmail.com>
2022-01-20 00:02:28 +0530

    [Shruti]Added:To calculate daily wage

--- EmployeeWage/EmployeeWage.sh
+++ EmployeeWage/EmployeeWage.sh
@@ -4,7 +4,11 @@ check_attend=$(( $RANDOM % 2));
 echo $check_attend;
 if [ $isPresent -eq $check_attend ]
 then
-	echo Employee is present;
+	WagePerHour=20;
+        FullDayHour=8;
+	emp_Daily_wage=$(($WagePerHour * $FullDayHour));
+        
+	echo "Employee wage is: $emp_Daily_wage";
 else
-	echo Employee is absent ;
-fi
+        echo "Employee is absent"
+fi    
