c75b31c5de1650b811df64ac64f405d080ae8911
ShrutiBelsare27 <shrutibelsare27@gmail.com>
2022-01-20 00:35:04 +0530

    [Shruti]Added:Refactor code to write function to get work hours

--- EmployeeWage/EmployeeWage.sh
+++ EmployeeWage/EmployeeWage.sh
@@ -1,38 +1,39 @@
 #!/bin/bash
+isPartTime=0;
+isFullTime=1;
 empRatePerHr=20;
 maxHrInMonth=100;
 numWorkingDay=20;
 total_working_hrs=0;
 total_working_days=0;
 check_attend=$(( $RANDOM % 2));
+function getworkhour()
+{
+    work=$1
+    case $work in 
+            0)
+                empHrs=8
+                echo $empHrs ;;
+            1)  
+                   empHrs=4
+                   echo $empHrs ;;
+            esac
+}
+
 case $check_attend in
 0)echo Employee is present;
-        while [[ $total_working_hrs -lt $maxHrInMonth && $total_working_days -lt  $numWorkingDay ]]
+        while (($total_working_hrs < $maxHrInMonth && $total_working_days < $numWorkingDay))
 	do
 	(( total_working_days++ ))
 	work=$(($RANDOM%2))
-	case $work in
-	0)empHrs=8;
-      	total_working_hrs=$(($total_working_hrs + $empHrs))
-        daily_wage=$(( empHrs* empRatePerHr ))
+	Hours="$(getworkhour $work)"
+     	total_working_hrs=$(($total_working_hrs + $Hours));
 	total_wage=$(($total_working_hrs*$empRatePerHr));
-	echo "Total Daily wage is $daily_wage"
-	echo "The full time  employee for the month is $total_wage"
-	;;
-	1)
-	empHrs=4;
-	total_working_hrs=$(($total_working_hrs + $empHrs))
-	daily_wage=$(( empHrs* empRatePerHr ))
-        total_wage=$(($total_working_hrs*$empRatePerHr));
-        echo "Total Daily wage is $daily_wage"
-	echo "The part time employee for the month is $total_wage"
-	;;
-	esac       
-     
+	echo  $total_wage
       done
-      echo $total_working_hrs;
-      echo  $total_working_days;
+       
        ;;
 1)
 echo emp is absent;;
 esac
+
