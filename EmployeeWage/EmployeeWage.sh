9e6213a22ab69616fa3119c776737ca92689f407
ShrutiBelsare27 <shrutibelsare27@gmail.com>
2022-01-20 00:51:49 +0530

    [Shruti]Added:store day and daily wage along with total wage

--- EmployeeWage/EmployeeWage.sh
+++ EmployeeWage/EmployeeWage.sh
@@ -6,16 +6,18 @@ total_working_hrs=0;
 total_working_days=0;
 total_wage=0;
 check_attend=$(( $RANDOM % 2));
+declare -A daily_wage;
+
 function getworkhour()
 {
     work=$1
     case $work in 
             0)
-                 empHrs=8
-                echo $empHrs;;
+                empHrs=8
+                echo $empHrs ;;
             1)  
-                  empHrs=4
-                 echo $empHrs;;
+                 empHrs=4
+                 echo $empHrs ;;
             esac
 }
 function get_Employee_wage()
@@ -31,12 +33,16 @@ case $check_attend in
         work=$(($RANDOM%2))
         empHour="$(getworkhour $work)"
         total_working_hrs=$(($total_working_hrs + $empHour));
-        daily_wage[$total_working_days]="$( get_Employee_wage  $empHour)"; 
-	total_wage=$(($(get_Employee_wage $total_working_hrs))) 
-	echo "${daily_wage[@]}   ${total_wage[@]}"
-      done
-       ;;
+        daily_wage["$total_working_days"]="$( get_Employee_wage  $empHour)"; 
+        total_wage=$(($(get_Employee_wage $total_working_hrs))) 
+        echo "day is: ${total_working_days}"      
+        echo "Daily wage is: ${daily_wage[@]}"
+        echo ${!daily_wage[@]}
+	echo "Total wage is : ${total_wage[@]}"
+       done
+      ;;
 1)
 echo emp is absent;;
 esac
+
 
