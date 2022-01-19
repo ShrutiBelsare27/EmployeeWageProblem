c2ad6b096da946c8a48d5fc5941d1e250c8422df
ShrutiBelsare27 <shrutibelsare27@gmail.com>
2022-01-20 00:09:05 +0530

    [Shruti]Added:To add part time employee and wage

--- EmployeeWage/EmployeeWage.sh
+++ EmployeeWage/EmployeeWage.sh
@@ -1,14 +1,24 @@
 #!/bin/bash -x
 isPresent=1;
+isPartTime=0;
+isFullTime=1;
+empRatePerHr=20;
 check_attend=$(( $RANDOM % 2));
 echo $check_attend;
 if [ $isPresent -eq $check_attend ]
 then
-	WagePerHour=20;
-        FullDayHour=8;
-	emp_Daily_wage=$(($WagePerHour * $FullDayHour));
-        
-	echo "Employee wage is: $emp_Daily_wage";
+echo Employee is present;
+work=$(($RANDOM%2))
+if [ $work -eq $isFullTime ]
+then
+empHrs=8;
+salary=$(($empHrs * $empRatePerHr));
+echo $salary;
 else
-        echo "Employee is absent"
-fi    
+empHrs=4;
+salary=$(($empHrs*$empRatePerHr));
+echo $salary;
+fi        
+else
+echo emp is absent;
+fi
